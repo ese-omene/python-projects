08b8e66a2f60995c754a395c250c49f4f9a0ed58
Eseroghene Omene <eomene@gmail.com>
2022-10-27 09:42:48 -0400

    weather api updated

--- weather.py
+++ weather.py
@@ -1,17 +1,28 @@
 import requests
+import datetime
+from datetime import datetime
 
-BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
+API_KEY = "f8503fcd89b993216a45165f67c43073"
 
 city = input("Enter city name: ")
 
-request_url = f"{BASE_URL}?q={city}$appid={API_KEY}&units=metric"
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}&units=metric"
 response = requests.get(request_url)
 
 if response.status_code == 200 :
     data = response.json()
     print(data)
+    weather = data['weather'][0]['description']
+    feels_like = data['main']['feels_like']
+    highs = data['main']['temp_max']
+    lows = data['main']['temp_min']
+    print(f"Weather forecast for to for the city of {city} :   ")
+    print(f"Weather conditions: {weather}")
+    print(f"Feels like: {feels_like}")
+    print(f"High: {highs}")
+    print(f"Low: {lows}")
 else:
     print("there has been an error")
 
-print('hello')
